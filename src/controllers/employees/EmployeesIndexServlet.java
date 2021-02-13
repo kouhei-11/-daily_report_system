3d66bf253e3863501865a2e01bae014888ff81ea
kouhei.yoshida <kanryu789654@gmail.com>
2021-02-13 15:12:55 +0900

    Add Employee create

--- src/controllers/employees/EmployeesIndexServlet.java
+++ src/controllers/employees/EmployeesIndexServlet.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import models.Employee;
 import utils.DBUtil;
+
 /**
  * Servlet implementation class EmployeesIndexServlet
  */
@@ -31,33 +32,35 @@ public class EmployeesIndexServlet extends HttpServlet {
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    EntityManager em = DBUtil.createEntityManager();
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
 
-    int page = 1;
-    try{
-        page = Integer.parseInt(request.getParameter("page"));
-    } catch(NumberFormatException e) { }
-    List<Employee> employees = em.createNamedQuery("getAllEmployees", Employee.class)
-                                 .setFirstResult(15 * (page - 1))
-                                 .setMaxResults(15)
-                                 .getResultList();
+        int page = 1;
+        try {
+            page = Integer.parseInt(request.getParameter("page"));
+        } catch (NumberFormatException e) {
+        }
+        List<Employee> employees = em.createNamedQuery("getAllEmployees", Employee.class)
+                .setFirstResult(15 * (page - 1))
+                .setMaxResults(15)
+                .getResultList();
 
-    long employees_count = (long)em.createNamedQuery("getEmployeesCount", Long.class)
-            .getSingleResult();
+        long employees_count = (long) em.createNamedQuery("getEmployeesCount", Long.class)
+                .getSingleResult();
 
-em.close();
+        em.close();
 
-request.setAttribute("employees", employees);
-request.setAttribute("employees_count", employees_count);
-request.setAttribute("page", page);
-if(request.getSession().getAttribute("flush") != null) {
-    request.setAttribute("flush", request.getSession().getAttribute("flush"));
-    request.getSession().removeAttribute("flush");
-}
+        request.setAttribute("employees", employees);
+        request.setAttribute("employees_count", employees_count);
+        request.setAttribute("page", page);
+        if (request.getSession().getAttribute("flush") != null) {
+            request.setAttribute("flush", request.getSession().getAttribute("flush"));
+            request.getSession().removeAttribute("flush");
+        }
 
-RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/employees/index.jsp");
-rd.forward(request, response);
-}
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/employees/index.jsp");
+        rd.forward(request, response);
+    }
 
 }
